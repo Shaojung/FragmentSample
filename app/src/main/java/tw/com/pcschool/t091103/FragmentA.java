16abc23903be2d8fecd831e89a56dfc11970a667
Shaojung <shaojung@gmail.com>
2016-09-11 16:32:42 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/t091103/FragmentA.java
@@ -0,0 +1,20 @@
+package tw.com.pcschool.t091103;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by teacher on 2016/9/11.
+ */
+public class FragmentA extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_a, container);
+        return v;
+    }
+}
---
+++ app/src/main/java/tw/com/pcschool/t091103/MainActivity.java
@@ -0,0 +1,21 @@
+package tw.com.pcschool.t091103;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1()
+    {
+
+    }
+    public void click2()
+    {
+
+    }
+}
