d94fb542353e1abb95f24b1db87ee4f46cfb51b4
Shaojung <shaojung@gmail.com>
2016-09-11 16:42:40 +0800

    add replace

--- app/src/main/java/tw/com/pcschool/t091103/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t091103/MainActivity.java
@@ -1,7 +1,9 @@
 package tw.com.pcschool.t091103;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,12 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void click1()
+    public void click1(View v)
     {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.my_frag_layout, new FragmentA());
+        ft.commit();
 
     }
-    public void click2()
+    public void click2(View v)
     {
-
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.my_frag_layout, new FragmentB());
+        ft.commit();
     }
 }
